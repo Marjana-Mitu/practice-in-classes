36e95c33365c5dcb4ab75a36770a6f93af35c629
marjana-mitu <marjanamitu19@gmail.com>
2024-11-05 11:08:09 +0600

    meaningful comment

---
+++ Nov5.js
@@ -0,0 +1,63 @@
+// var i;
+
+// for (i=5; i<=11; i++){
+//     console.log("hello there");
+//     console.log( "next number:", i);
+
+// }
+
+
+// var n;
+// var sum;
+// var s;
+
+// for (n=100; n<=200; n++){
+//     console.log(n);
+// sum  = sum + n 
+// count = count + 1
+// }
+//  
+
+
+// var sum = 0;
+// var count = 0;
+
+// for ( i = 100; i <= 200; i++) {
+//     sum += i;
+//     count++;
+// }
+
+// var average = sum / count;
+
+// console.log("Sum:", sum);
+// console.log("Average:", average);
+
+
+// var firstNumber = parseInt(prompt("Enter the first number for the range"));
+// var LastNumber = parseInt(prompt("Enter the last number for the range"));
+
+
+// var sum = 0;
+// var count = 0;
+
+//  for ( i = firstNumber; i <= LastNumber; i++){
+//     sum += i;
+// count++;
+// }
+// var average = sum / count;
+// console.log("Sum:", sum);
+// console.log("Average:", average);
+
+
+var quizsum = 0;
+var quizcount = 0;
+var quiznumbers = parseInt(prompt("Enter the  number of quizes you attented for the course"));
+
+
+ for (i=quiznumbers; i<=quiznumbers; i++){
+    var quizmarks = parseInt(prompt("Enter the  marks of quizes you attented for the course);
+    sum +=i;
+    count++;
+ }
+ var average = sum / count;
+ console.log("Your attented", quiznumbers, "Average:", average);
